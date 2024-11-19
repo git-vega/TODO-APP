633886fb0d15549487dc297fb180ac407b3cdf07
michaelrezzavega <rezzamichael@gmail.com>
2024-11-19 23:51:39 +0100

    TODO-APP:Version_01.00.02-localStorage-Speicher in Browser

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -13,18 +13,33 @@ export class TodoComponent {
   newTodo: string = '';
   todos: { task: string, done: boolean }[] = [];
 
+  ngOnInit() {
+    // Laden der Todos aus LocalStorage
+    const savedTodos = localStorage.getItem('todos'); // prüfen ob todos im LocalStorage vorhanden sind
+    if(savedTodos){
+      this.todos = JSON.parse(savedTodos);  // Lädt die gespeicherten Produkte in die Liste
+    }
+  }
+
   addTodo() {
     if (this.newTodo.trim()) {
       this.todos.push({ task: this.newTodo, done: false });
       this.newTodo = '';
+      this.saveTodosToLocalStorage();
     }
   }
 
   removeTodo(index: number) {
     this.todos.splice(index, 1);
+    this.saveTodosToLocalStorage();
   }
 
   toggleDone(index: number) {
     this.todos[index].done = !this.todos[index].done;
+  }
+
+  // Methode zum Speichern in LocalStorage
+  saveTodosToLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
